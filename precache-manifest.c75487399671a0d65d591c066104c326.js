0ce45609d71b2f164d82e5c0010afd579fbf20af
Reza <shadow.rexa@gmail.com>
2020-02-05 14:51:53 +0330

    Updates

---
+++ precache-manifest.c75487399671a0d65d591c066104c326.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fb66c7ac4fb766cd43df117c56faf77",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "f8b1040304f2d6eee923",
+    "url": "/monsters/static/css/main.e5e7aa33.chunk.css"
+  },
+  {
+    "revision": "0a153d6a722481fef969",
+    "url": "/monsters/static/js/2.0e7e587d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters/static/js/2.0e7e587d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f8b1040304f2d6eee923",
+    "url": "/monsters/static/js/main.8919eb08.chunk.js"
+  },
+  {
+    "revision": "b5d546aaf7ad34e6acf8",
+    "url": "/monsters/static/js/runtime-main.b336abb4.js"
+  }
+]);
